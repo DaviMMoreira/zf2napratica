15b2c34d159d0ce324cf7b545ea2f85967fe578e
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2014-01-31 22:09:39 -0200

    Criando a entidade Application\Model\Post.

---
+++ module/Application/src/Application/Model/Post.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Application\Model;
+
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+use Core\Model\Entity;
+
+
+/**
+ * Description of Post
+ *
+ * @author Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
+ */
+class Post extends Entity {    
+    /**
+     * @var string
+     */
+    protected $tableName = 'post';
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $title;    
+    /**
+     * @var string
+     */
+    protected $description;
+    /**
+     * @var datetime
+     */
+    protected $post_date;
+    
+    public function getInputFilter() {
+        if(!$this->inputFilter){
+            $inputFilter = new InputFilter();
+            $factory = new InputFactory();
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'       => 'id',
+                'required'   => true,
+                'filters'    => array(
+                    array( 'name' => 'Int' )
+                )
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'       => 'title',
+                'required'   => true,
+                'filters'    => array(
+                    array( 'name' => 'StripTags' ),
+                    array( 'name' => 'StringTrim' )
+                ),
+                'validators' => array(
+                    array(
+                        'name'      => 'StringLength' ,
+                        'options'   => array(
+                            'encoding'  => 'UTF-8',
+                            'min'       =>   1,
+                            'max'       => 100,
+                        )
+                    )
+                )
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'       => 'description',
+                'required'   => true,
+                'filters'    => array(
+                    array( 'name' => 'StripTags' ),
+                    array( 'name' => 'StringTrim' )
+                )
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'       => 'post_date',
+                'required'   => false,
+                'filters'    => array(
+                    array( 'name' => 'StripTags' ),
+                    array( 'name' => 'StringTrim' )
+                )
+            )));
+            
+            $this->inputFilter = $inputFilter;
+        }
+        
+        return $this->inputFilter;
+    }
+}
