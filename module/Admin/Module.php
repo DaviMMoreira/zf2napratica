115732e5f0f41e4adc81d5acd5073d78fa0898b8
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2014-05-04 00:21:29 -0300

    Adicionando listener para início de validação de autorização, no módulo Admin.

--- module/Admin/Module.php
+++ module/Admin/Module.php
@@ -22,4 +22,48 @@ class Module
     {
         return include __DIR__ . '/config/module.config.php';
     }
+    
+    /**
+     * @param MvcEvent $e
+     */
+    public function onBootstrap($e)
+    {
+        $moduleManager = $e->getApplication()
+                            ->getServiceManager()
+                            ->get('modulemanager');
+        $sharedEvents = $moduleManager->getEventManager()
+                            ->getSharedManager();
+        
+        $sharedEvents->attach(
+            'Zend\Mvc\Controller\AbstractActionController',
+            \Zend\Mvc\MvcEvent::EVENT_DISPATCH,
+            array ( $this, 'mvcPreDispatch' ),
+            100
+        );
+    }
+    
+    /**
+     * @param MvcEvent $event
+     * @return boolean
+     */
+    public function mvcPreDispatch($event)
+    {
+        $di         = $event->getTarget()->getServiceLocator();
+        $routeMatch = $event->getRouteMatch();
+        
+        $moduleName     = $routeMatch->getParam('module');
+        $controllerName = $routeMatch->getParam('controller');
+        
+        if ( $moduleName == 'admin' && $controllerName != 'Admin\Controller\Auth' )
+        {
+            $authService = $di->get('Admin\Service\Auth');
+            if ( !$authService->authorize() )
+            {
+                $redirect = $event->getTarget()->redirect();
+                $redirect->toUrl('/admin/auth');
+            }
+        }
+        
+        return true;
+    }
 }
