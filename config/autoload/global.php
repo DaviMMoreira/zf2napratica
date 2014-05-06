03c2bbd34c52ab0f2e0334837ab87bc85d0fcc3e
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2014-05-05 22:44:10 -0300

    Criando estrutura de regras de Acl.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,40 @@ return array(
         'driver_options' => array (
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         )
+    ),
+    'acl' => array(
+        'roles' => array(
+            'visitor' => null,
+            'writer'  => 'visitor',
+            'admin'   => 'writer'
+        ),
+        'resources' => array(
+            'Application\Controller\Index.index',
+            'Admin\Controller\Index.save',
+            'Admin\Controller\Index.delete',
+            'Admin\Controller\Auth.index',
+            'Admin\Controller\Auth.login',
+            'Admin\Controller\Auth.logout',
+        ),
+        'privilege' => array(
+            'visitor' => array(
+                'allow' => array(
+                    'Application\Controller\Index.index',
+                    'Admin\Controller\Auth.index',
+                    'Admin\Controller\Auth.login',
+                    'Admin\Controller\Auth.logout',
+                )
+            ),
+            'writer' => array(
+                'allow' => array(
+                    'Admin\Controller\Index.save',
+                )
+            ),
+            'admin' => array(
+                'allow' => array(
+                    'Admin\Controller\Index.delete',
+                )
+            ),
+        )
     )
 );
